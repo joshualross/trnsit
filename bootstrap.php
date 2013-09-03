c5eaf36ea46883e7d54aa67ee0dd09ebb5ed3d44
Joshua Ross <joshualross@gmail.com>
2013-09-02 23:07:46 -0700

    cleanup error handling/messages

--- bootstrap.php
+++ bootstrap.php
@@ -4,10 +4,7 @@
  */
 define('__BASE__', __DIR__ . DIRECTORY_SEPARATOR);
 set_include_path(__BASE__ . PATH_SEPARATOR . get_include_path());
-
-
 ini_set('error_reporting', E_ALL);
-ini_set('display_errors', true);
 ini_set('error_log', __BASE__ . 'log' . DIRECTORY_SEPARATOR . 'error.log');
 
 require_once __BASE__ . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
